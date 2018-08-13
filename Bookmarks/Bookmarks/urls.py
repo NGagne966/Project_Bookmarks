e8899bf1b1d9edf4c056b081625e473b04bd6973
Nicolas Gagne <ngagne966@gmail.com>
2018-08-13 10:34:05 -0400

    Blank rest API

--- Bookmarks/Bookmarks/urls.py
+++ Bookmarks/Bookmarks/urls.py
@@ -15,10 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
 
 from AppBookmarks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+]
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
     path('', views.index, name='index'),
+    url(r'api', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
