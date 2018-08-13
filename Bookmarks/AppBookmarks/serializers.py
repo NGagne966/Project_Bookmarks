21979c9d3428e88f20d505b4c4aefed3d88ddd2f
Nicolas Gagne <ngagne966@gmail.com>
2018-08-13 15:24:16 +0000

    Update serializers.py

--- Bookmarks/AppBookmarks/serializers.py
+++ Bookmarks/AppBookmarks/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-
+# TEST pull up
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
