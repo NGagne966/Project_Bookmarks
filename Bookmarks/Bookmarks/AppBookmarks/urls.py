1d02d4cb74bd9ed767a7a8ceb94cdca1c363bba2
Nicolas Gagne <ngagne966@gmail.com>
2018-08-15 06:38:34 -0400

    Made some change

--- Bookmarks/Bookmarks/AppBookmarks/urls.py
+++ Bookmarks/Bookmarks/AppBookmarks/urls.py
@@ -7,7 +7,7 @@ router_app_bookmarks = routers.DefaultRouter()
 router_app_bookmarks.register(r'users', views.UserViewSet)
 router_app_bookmarks.register(r'bookmarks', views.BookmarksViewSet)
 router_app_bookmarks.register(r'new_bookmarks', views.CreateBookmarkViewSet, base_name='new_bookmarks')
-
+# I made some change
 
 url_app_bookmarks = [
 
