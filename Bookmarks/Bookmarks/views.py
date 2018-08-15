35af1cc0c3b3ea08b09918531927261cc125f654
Nicolas Gagne <ngagne966@gmail.com>
2018-08-15 09:00:41 -0400

    Created Landing page Connect static, Landing page need some work.

--- Bookmarks/Bookmarks/views.py
+++ Bookmarks/Bookmarks/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the Bookmarks index.")
-    return render(request, 'bookmarks_list.html', {})
+    #return render(request, 'bookmarks_list.html', {})
+    return render(request, 'index.html', {})
